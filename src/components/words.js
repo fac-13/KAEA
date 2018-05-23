f74948da112334eaafe92d4e31ff49f1f4259051
eadehemingway <eade.hemingway@gmail.com>
2018-05-23 17:04:06 +0100

    try to make the word and defintiions render

--- src/components/words.js
+++ src/components/words.js
@@ -6,23 +6,39 @@ console.log(apiKey);
 const url = `https://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=true&excludePartOfSpeech=%20proper-noun&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=10&maxLength=-1&limit=4&api_key=${apiKey}`;
 const urlDefs = ``;
 
+const Definitions = props => <li className="definition">{props.definition}</li>;
+
 class Words extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      words: []
+      words: null
     };
   }
   componentDidMount() {
     getWords(url).then(data => {
-      console.log(data);
-      getDefs;
+      console.log('dATA', data);
+      this.setState({ words: data });
+      console.log('WORDS', this.state.words);
     });
   }
   render() {
+    if (!this.state.words) {
+      return <h3> ...loading</h3>;
+    }
+
+    const keys = Object.keys(this.state.words);
+    const values = Object.values(this.state.words);
+    console.log('values', values);
+    console.log('WORDStwo', this.state.words);
+    console.log('KEYS', keys);
+
     return (
       <div>
-        <h2>Hellooooo</h2>
+        <h2>{keys[0]}</h2>
+        <ul className="definitionList">
+          {keys.map(key => <li key={key}> {this.state.words[key]}</li>)}
+        </ul>
       </div>
     );
   }
--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,6 +1,5 @@
 import { apiKey } from './../../key.js';
 const url = `https://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=true&excludePartOfSpeech=%20proper-noun&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&limit=4&api_key=${apiKey}`;
-const obj = {};
 
 const formatUrl = word => {
   return `https://api.wordnik.com/v4/word.json/${word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=${apiKey}`;
@@ -15,6 +14,7 @@ const checkResponse = response => {
 };
 
 export const getWords = url => {
+  const obj = {};
   return fetch(`${url}`)
     .then(checkResponse)
     .then(resp => {
@@ -33,7 +33,3 @@ export const getWords = url => {
       throw new Error(`fetch getData failed ${err}`);
     });
 };
-
-// export const getDefs = async () => {
-//   const responses = await Promise.all([fetch(formatUrl())]);
-// };

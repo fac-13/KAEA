bdf1cf762e8d7033b514324a6a9c1cb433914712
eadehemingway <eade.hemingway@gmail.com>
2018-05-23 15:35:02 +0100

    alter getData function so that it gets the definitions as well as the words

--- src/components/words.js
+++ src/components/words.js
@@ -1,27 +1,31 @@
-import React from "react";
-import { getData } from "../utils/getData";
+import React from 'react';
+import { getWords, getDefs } from '../utils/getData';
 // import '../../assets/styles.css'
-
 import { apiKey } from './../../key.js';
 console.log(apiKey);
 const url = `https://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=true&excludePartOfSpeech=%20proper-noun&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=10&maxLength=-1&limit=4&api_key=${apiKey}`;
-
+const urlDefs = ``;
 
 class Words extends React.Component {
-    constructor(props){
-        super(props)
-            this.state = {
-                words: [],
-            }
-        }
-    componentDidMount() {
-        getData(url).then(data => console.log(data));
-    }
-    render() {
-        return (<div>
-          <h2>Hellooooo</h2>
-        </div>)
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      words: []
+    };
   }
+  componentDidMount() {
+    getWords(url).then(data => {
+      console.log(data);
+      getDefs;
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h2>Hellooooo</h2>
+      </div>
+    );
+  }
+}
 
 export default Words;
--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,6 +1,10 @@
-// import { apiKey } from './../../key.js';
+import { apiKey } from './../../key.js';
+const url = `https://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=true&excludePartOfSpeech=%20proper-noun&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&limit=4&api_key=${apiKey}`;
+const obj = {};
 
-// const url = `https://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=true&excludePartOfSpeech=%20proper-noun&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&limit=4&api_key=${apiKey}`;
+const formatUrl = word => {
+  return `https://api.wordnik.com/v4/word.json/${word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=${apiKey}`;
+};
 
 const checkResponse = response => {
   if (response.status !== 200) {
@@ -10,11 +14,26 @@ const checkResponse = response => {
   return response.json();
 };
 
-export const getData = url => {
-  console.log(url);
+export const getWords = url => {
   return fetch(`${url}`)
     .then(checkResponse)
+    .then(resp => {
+      resp.forEach(object => {
+        const word = object.word;
+        obj[word] = '';
+        fetch(formatUrl(word))
+          .then(checkResponse)
+          .then(result => {
+            obj[word] = result[0].text;
+          });
+      });
+      return obj;
+    })
     .catch(err => {
       throw new Error(`fetch getData failed ${err}`);
     });
 };
+
+// export const getDefs = async () => {
+//   const responses = await Promise.all([fetch(formatUrl())]);
+// };
